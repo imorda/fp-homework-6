2c8f735743cc2da6a9b4a8ed9df36dfe6c3738ad
imorda <btimka1@gmail.com>
2024-01-06 09:29:26 +0700

    T2 seems to be done

--- hw6/src/HW6/T2.hs
+++ hw6/src/HW6/T2.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module HW6.T2
   ( TSet
@@ -10,12 +11,19 @@ module HW6.T2
   , Delete
   ) where
 
-import GHC.TypeLits
+import           GHC.TypeLits
 
 type TSet = [Symbol]
 
 type family Contains (name :: Symbol) (set :: TSet) :: Bool where
+  Contains _ '[] = 'False
+  Contains val (val:xs) = 'True
+  Contains val (_:xs) = Contains val xs
 
 type family Delete (name :: Symbol) (set :: TSet) :: TSet where
+  Delete _ '[] = '[]
+  Delete val (val:xs) = xs
+  Delete val (x:xs) = x : Delete val xs
 
 type family Add (v :: Symbol) (set :: TSet) :: TSet where
+  Add val lst = val : Delete val lst
